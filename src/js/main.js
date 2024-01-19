2aba6613ad33c5fdf41096de203c00250b6263b4
Ferrari050 <jvm.ferrari@hotmail.com>
2024-01-19 08:57:27 -0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-function tocaSom () {
+/*function tocaSom () {
     document.querySelector('audio').play();
 }
 
@@ -10,4 +10,25 @@ while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = tocaSom;
     contador++;
 
-}
+}*/
+document.addEventListener('DOMContentLoaded', function () {
+    // Seletor para todos os botões
+    var buttons = document.querySelectorAll('button');
+
+    // Adicionar evento de clique a cada botão
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            // Obter o ID do botão clicado
+            var buttonId = this.id;
+
+            // Obter o elemento de áudio correspondente ao botão clicado
+            var audioElement = document.getElementById('som' + buttonId.slice(-1));
+
+            // Verificar se o elemento de áudio existe
+            if (audioElement) {
+                // Reproduzir o som
+                audioElement.play();
+            }
+        });
+    });
+});
